a912e05ad4a4a52f071979be918d56d4dd84c7a7
Martin Ennemoser <martin.ennemoser@gmx.at>
2020-10-01 11:32:22 +0200

    Added tip.

--- java/martinennemoser/uebungen/Exercise 1/Main.java
+++ java/martinennemoser/uebungen/Exercise 1/Main.java
@@ -55,14 +55,14 @@ public class Main {
         // Tipp: Du kannst das mit einer Schleife oder zwei
 		// ineinander geschachtelten Schleifen lösen
 		
-	////// Exercise 5:
+        ////// Exercise 5:
         // Erzeuge auf der Konsole folgendes Muster:
-	// XOOOOOX
+        // XOOOOOX
         // OXOOOXO
-	// OOXOXOO
-	// OOOXOOO
+        // OOXOXOO
+        // OOOXOOO
         // Tipp: Die X sind in V-Form angeordnet
-
+		// Tipp: Verwende zwei ineinander geschachtelte Schleifen.
 
 
     }
