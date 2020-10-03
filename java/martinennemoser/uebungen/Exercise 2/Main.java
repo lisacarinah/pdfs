c94fc0c8dfe6b3123a15bb7da582e2735ebe44f3
Martin Ennemoser <martin.ennemoser@gmx.at>
2020-10-03 09:02:40 +0200

    Exercise 2

---
+++ java/martinennemoser/uebungen/Exercise 2/Main.java
@@ -0,0 +1,57 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        ////// Exercise 1:
+        // Folgendes Array steht zur Verfügung:
+        int [] arr1 = {0, 1, 2, 3, 4, 5};
+        // Erzeuge ein zweites Array arr2, welches den Inhalt dieses Arrays
+        // nimmt, aber in umgedrehter Reihenfolge.
+        // Das Array soll also folgendermaßen aussehen:
+        // arr2: 5, 4, 3, 2, 1, 0
+        // Gib dieses Array folgendermaßen auf der Konsole aus:
+        // 5 4 3 2 1 0
+
+
+        ////// Exercise 2:
+        // Erzeuge ein String-Array mit 12 Fächern.
+        // Speichere in jedem Fach den Namen eines Kalendermonats.
+        // z.B. monats_array[2] = "März"; monats_array[11] = "Dezember"
+        // Gib dieses Array folgendermaßen aus:
+        // Jänner, Feber, März, April, Mai, Juni, Juli, August, September, Oktober, November, Dezember
+
+        ////// Exercise 3:
+        // Erzeuge ein int-Array mit 3 Fächern.
+        // Das Array soll folgende Werte enthalten: [3, 1, 5]
+        // Sortiere dieses Array.
+        // Der Inhalt nach dem Sortieren soll folgender sein: [1, 3, 5]
+        // Tipp: Verwende Hilfsvariablen
+
+        ////// Exercise 4:
+        // Erzeuge ein Array mit folgendem Muster:
+        // 1, 3, 9, 27, 81
+        // Tipp: Es gibt zwei Wege diese Zahlen zu erzeugen:
+        // entweder als hintereinander-folgende Multiplikation: 3*3*3...
+        // oder du nimmst die Exponential-Methode: Math.pow(3, i)
+        // z.B. int val = (int) Math.pow(3, 0);
+
+        ////// Exercise 5:
+        // Erzeuge ein eindimensionales Muster aus
+        // "O" und "X". Jedem zweiten "X" muss ein "O" folgen. Das Muster muss mit einem
+        // "O" starten.
+        // Die Anzahl an Gesamtzeichen soll vom Benutzer konfigurierbar sein,
+        // muss jedoch zwischen 4 und 30 liegen.
+        // Verwenden zur Erstellung des Musters ein Array.
+        // Gültige Beispiele wären:
+        // Beispiel 1:
+        // OXXOXXOXXOXXOXXOX
+        // Beispiel 2:
+        // OXXOXXOX
+        // Beispiel 3:
+        // OXXO
+
+
+
+
+    }
+}
