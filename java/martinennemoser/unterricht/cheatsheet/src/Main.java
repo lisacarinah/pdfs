e62832d73831a1674afcc4f6912a6a28b92cbaf8
martin <martin.ennemoser@xemedo.com>
2020-10-07 15:33:10 +0200

    Added gitignore and cheatsheet.

---
+++ java/martinennemoser/unterricht/cheatsheet/src/Main.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int[] array = new int[5];  // Array mit 5 Elementen
+
+        // gesamtes Array ausgeben
+        System.out.println(Arrays.toString(array));
+
+        // spezifisches Element ausgeben (z.B. das 4e):
+        System.out.println(array[4]);
+    }
+}
