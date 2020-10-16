e608981db1c704853778f4d206fa93889e1efcfc
NMO13 <martin.ennemoser@gmx.at>
2020-10-16 22:54:05 +0200

    Added exercise.

---
+++ java/martinennemoser/Enums/src/Main.java
@@ -0,0 +1,79 @@
+import javax.print.DocFlavor;
+import javax.xml.stream.FactoryConfigurationError;
+import java.util.Arrays;
+
+public class Main {
+   public static void main(String[] args) {
+       ///// 1. enums
+       // Erzeuge ein Enum mit dem Namen "Wochentage"
+       // Ereuge eine Variable von diesem Typ.
+       // Weise der Variable einen beliebigen Wochentag zu.
+       // Wenn der Tag Montag ist, dann gib auf der Konsole aus: "Am Montag bin ich müde"
+       // Wenn der Tag Dienstag, Mittwoch oder Donnerstag ist, dann gib auf der Konsole aus:
+       // "Bald ist wieder Wochenende"
+       // Wenn der Tag Freitag ist, dann gib auf der Konsole aus: "Geschafft, es ist Wochenende"
+       // Wenn der Tag Samstag oder Sonntag ist, dann gib auf der Konsole aus:
+       // "Ich genieße das Wochenende"
+       // Verwende wenn möglich eine switch/case-Anweisung.
+
+       ///// 2. Sortieren
+       // Schreibe ein Programm, welches 5 verschiedene Wörter, welche der Benutzer
+       // eingeben kann einliest und anschließend sortiert.
+       // Wie wir gelernt haben, können Strings folgendermaßen eingelesen werden:
+       // Scanner scan = new Scanner(System.in);
+       // String wort = scan.nextLine();
+
+       // Speichere die Wörter in einem Array mit dem Namen "woerter".
+       // Sortiere dieses Array, indem du den Bubble-Sort Algorithmus implementierst.
+       // Tipp: Du wirst für deinen Bubble-Sort Algorithmus folgendes benötigen, um
+       // feststellen zu können, ob 2 Wörter getauscht werden müssen:
+       // if (woerter[i].compareTo(woerter[j])>0)
+       // {
+       //    // Tausche woerter[i] und woerter[j] hier (mit einer Hilfsvariable)
+       // }
+
+       ///// 3. Funktionen
+       // Implementiere eine Funktion, welche 2 Zahlen addiert und das
+       // Ergebnis zurückgibt.
+       // z = addieren(5, 3)
+       // System.out.println(z) // 8
+       // Tip: Der Datentyp ist immer int
+
+       // Implementiere eine Funktion, welche 2 Zahlen multipliziert und das
+       // Ergebnis zurückgibt.
+       // z = addieren(5, 3)
+       // System.out.println(z) // 15
+       // Tip: Der Datentyp ist immer int
+
+       // Implementiere eine Funktion, welche die Länge einer multiplikativen
+       // Kette entgegen nimmt, diese Kette berechnet und das Ergebnis
+       // zurückliefert.
+       // Eine multiplikative Kette ist das Produkt mehrer aufsteigender Faktoren.
+       // Beispiele:
+       // z.B:
+       // Eine multiplikative Kette mit der Länge 2 hat folgende Form:
+       // 1 * 2 = 2
+       // Eine multiplikative Kette mit der Länge 3 hat folgende Form:
+       // 1 * 2 * 3 = 6
+       // Eine multiplikative Kette mit der Länge 5 hat folgende Form:
+       // 1 * 2 * 3 * 4 * 5 = 120
+       // Eine multiplikative Kette mit der Länge 1 hat folgende Form:
+       // 1 = 1
+       // Spezialfall: Das Ergebnis einer multiplikativen Kette mit der
+       // Länge 0 ist immer 1!
+
+       // Funktionsaufruf Beispiele:
+       // z = multChain(3)
+       // System.out.println(z) // 6
+
+       // z = multChain(0)
+       // System.out.println(z) // 1
+
+       // z = multChain(5)
+       // System.out.println(z) // 120
+       // Tip: Der Datentyp ist immer int
+
+
+
+    }
+}
