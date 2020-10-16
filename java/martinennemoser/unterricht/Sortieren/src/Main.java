ba80379c80744b1f7aecb8515b12d3630aec88c6
NMO13 <martin.ennemoser@gmx.at>
2020-10-16 23:02:14 +0200

    Cleanup.

--- java/martinennemoser/unterricht/Sortieren/src/Main.java
+++ java/martinennemoser/unterricht/Sortieren/src/Main.java
@@ -33,4 +33,9 @@ public class Main {
 
         System.out.println(Arrays.toString(zuSortierend));
     }
+    
+    // Sortieren in Java
+    int[] arr = {13, 7, 6, 45, 21, 9, 101, 102};
+    Arrays.sort(arr);
+    System.out.println(Arrays.toString(arr));
 }
