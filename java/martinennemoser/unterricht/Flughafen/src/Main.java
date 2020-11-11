5b9d122b85bd5ddc069b403f200b4f21e3e3b1de
NMO13 <martin.ennemoser@gmx.at>
2020-11-11 13:21:20 +0100

    Added files.

---
+++ java/martinennemoser/unterricht/Flughafen/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    
+    //  verschieden Flughäfen
+    
+    // Flughafen Linz
+    // mehrere Flugzeuge mit mehreren Presonen
+
+    // Flughafen Wien
+    // mehrere Flugzeuge mit mehreren Presonen
+}
