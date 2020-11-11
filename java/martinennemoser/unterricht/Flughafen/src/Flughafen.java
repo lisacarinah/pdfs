66ffd9103836f90a8fc7f77011883743b9dc28f1
NMO13 <martin.ennemoser@gmx.at>
2020-11-11 13:20:58 +0100

    Added files.

---
+++ java/martinennemoser/unterricht/Flughafen/src/Flughafen.java
@@ -0,0 +1,13 @@
+public class Flughafen {
+
+    // Attribute
+    // Namen
+    // Liste an Personen, die sich momentan am Flughafen befinden
+    // Liste an Flugzeugen
+
+    // Methoden
+    // Flugzeug Landet
+    // Flugzeug hebt ab
+    // Person betritt Flughafen
+    // Person verlässt flughafen
+}
---
+++ java/martinennemoser/unterricht/Flughafen/src/Person.java
@@ -0,0 +1,12 @@
+public class Person {
+    
+    // Felder
+    // Status (Enum: KAUFT_GESCHENK, WARTET_AM_GATE, RUFT_TAXI, IST_AM_BOARDING
+    
+    
+    // Methoden
+    // kauft Geschenk (nur möglich, wenn Person am Flughafen ist)
+    // wartet am Gate (nur möglich, wenn Person am Flughafen ist)
+    
+    // ruft Taxi (nur möglich, nachdem Person Flughafen verlassen hat)
+}
