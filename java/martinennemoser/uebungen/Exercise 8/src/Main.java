4e2f5554302fdc269a4076bb3c993e9e728c6da3
NMO13 <martin.ennemoser@gmx.at>
2020-12-03 15:29:19 +0100

    Added example code.

--- java/martinennemoser/uebungen/Exercise 8/src/Main.java
+++ java/martinennemoser/uebungen/Exercise 8/src/Main.java
@@ -7,5 +7,11 @@ public class Main {
         calculator.subtract(a, b);
         calculator.multiply(a, b);
         calculator.divide(a, b);
+
+        // todo
+        // Vector x = new Vector(4, 5, 6);
+        // Vector y = new Vector(4, 5, 6);
+        // calculator.add(x, y);
+        // calculator.subtract(x, y);
     }
 }
